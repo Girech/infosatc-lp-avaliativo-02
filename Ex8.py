77122c475cf469b1539ebbe2d86d649be2578a86
Girech <88850410+Girech@users.noreply.github.com>
2021-09-09 16:29:34 -0300

    Criação Ex8

---
+++ Ex8.py
@@ -0,0 +1,5 @@
+componentes = ["Placa-mãe Asus ROG STRIX Z490-G GAMING","Processador Intel Core i9-10850K","Gabinete Cooler Master MasterCase SL600M MCM-SL600M-SGNN-S00","Placa de Vídeo EVGA NVIDIA GeForce RTX 2080 Ti","FONTE CORSAIR HX SERIES MODULAR HX1000 1000W 80 PLUS PLATINUM","Corsair Vengeance RGB Pro 64 GB (2 x 32 GB)","SSD Corsair Force Series MP600, 2TB","WATER COOLER NZXT KRAKEN Z73 360MM"]
+#A
+componentes.remove("Gabinete Cooler Master MasterCase SL600M MCM-SL600M-SGNN-S00")
+componentes.remove("Placa de Vídeo EVGA NVIDIA GeForce RTX 2080 Ti")
+print(componentes)
