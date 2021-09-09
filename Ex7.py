699c0b15f649a06313c1bde7176c87dc21673993
Girech <88850410+Girech@users.noreply.github.com>
2021-09-09 16:29:25 -0300

    Criação Ex7

---
+++ Ex7.py
@@ -0,0 +1,16 @@
+#A
+filmes = ["Harry Potter", "Ilha do Medo"]
+jogos = ["Assetto Corsa", "Forza Horizon 5"]
+livros = ["Pai Rico Pai Pobre", "A Cabana"]
+esporte = ["Futebol", "Volei"]
+#B
+listas = [filmes,  jogos,  livros,  esporte]
+#C
+print(livros[0])
+#D
+del esporte[1]
+#E
+disciplinas = ["Matemática", "Física"]
+listas = listas + disciplinas
+
+print(listas)
